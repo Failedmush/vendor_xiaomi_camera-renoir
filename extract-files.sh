e3efef0cfb248eb67f7b4a349b76914c228be575
Karan Parashar <karan@pixelos.net>
2024-08-23 01:44:50 +0200

    Revert "lisa-miuicamera: Patch apk to change default gallery"
    This code doesn't exist anymore.
    
    This reverts commit 8f63cee4cf2ccae49174c75324a12d82cf00665f.
    
    Change-Id: Ic6039ac456bcd30b659fabd60b856771dc1678ca

--- extract-files.sh
+++ extract-files.sh
@@ -62,12 +62,7 @@ function blob_fixup() {
             patchelf --remove-needed libhidltransport.so "${2}"
             ;;
         system/priv-app/MiuiCamera/MiuiCamera.apk)
-            tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
-            apktool d -q "$2" -o "$tmp_dir" -f
-            grep -rl "com.miui.gallery" "$tmp_dir" | xargs sed -i 's|"com.miui.gallery"|"com.google.android.apps.photos"|g'
-            apktool b -q "$tmp_dir" -o "$2"
-            rm -rf "$tmp_dir"
-            split --bytes=20M -d "$2" "$2".part
+            split --bytes=20M -d "${2}" "${2}".part
             ;;
     esac
 }
